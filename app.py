9fab9a4752ebb5c818af76801b6f32b153ba2308
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2019-10-14 01:10:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('stopwords')
+nltk.download('punkt')
 
 LANGUAGE = "english"
 SENTENCES_COUNT = 10
